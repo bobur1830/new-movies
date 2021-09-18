49b4e9f6f83453528bfe6c85f308d41906407227
Bobur <boburtoshxoja@gmail.com>
2021-09-18 15:25:11 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+let imgBx = document.querySelectorAll('.imgBx');
+imgBx.forEach(popup => popup.addEventListener('click', () =>{
+    popup.classList.toggle('active');
+}));
+
+
+function toggleMenu() {
+    const menuToggle = document.querySelector('.toggle');
+    const navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+
+}
